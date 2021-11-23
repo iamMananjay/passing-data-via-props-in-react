0c2f59fbeec0ab0305add353eaf68b5c787b3cbd
Mananjay Shrestha <Mananjaysth111@gmail.com>
2021-11-23 11:25:00 +0545

    passing data through props

--- src/App.js
+++ src/App.js
@@ -3,9 +3,46 @@ import Expenses from "./Components/Expenses";
 import "./App.css";
 
 const App = () => {
+  const overallExpensenDetail = [
+    {
+      id: 1,
+      currentDate: new Date(),
+      expenseTitle: "School Fee",
+      expensePrice: 300,
+    },
+    {
+      id: 2,
+      currentDate: new Date(),
+      expenseTitle: "Insurance",
+      expensePrice: 600,
+    },
+    {
+      id: 3,
+      currentDate: new Date(),
+      expenseTitle: "College Fee",
+      expensePrice: 500,
+    },
+    {
+      id: 4,
+      currentDate: new Date(),
+      expenseTitle: "House Rent",
+      expensePrice: 400,
+    },
+  ];
+
   return (
     <div>
-      <Expenses />
+      {/* pass the data from app componet to expenses componet */}
+      {overallExpensenDetail.map((items) => {
+        return (
+          <Expenses
+            time={items.currentDate}
+            title={items.expenseTitle}
+            price={items.expensePrice}
+          />
+        );
+      })}
+      {/* as we know componet is reuable so we can make component multiple time */}
     </div>
   );
 };
--- src/Components/Expenses.js
+++ src/Components/Expenses.js
@@ -1,13 +1,24 @@
 import React from "react";
 import "./Expenses.css";
-const Expenses = () => {
+const Expenses = (props) => {
+  // we received the data from one componet to another inside the function as parameter which store all the data as Object
+  // where here all data are pass inside the props object . we can set any name here instead of props
+  // now access the value like as object properties
+  // {props.title}
+
+  // Now for date we have to convert the date in string from
+  const currentYear = props.time.getFullYear();
+  const currentMonth = props.time.getMonth();
+  const currentDate = props.time.getDate();
   return (
     <>
-      <h1>Daily Expenses</h1>
       <div className="container">
-        <div className="expense-date">october 5</div>
-        <div className="expense-title">School Fee</div>
-        <div className="expense-price">$300</div>
+        <div className="expense-date">
+          <div>{currentYear}</div>-<div>{currentMonth}</div>-
+          <div>{currentDate}</div>
+        </div>
+        <div className="expense-title">{props.title}</div>
+        <div className="expense-price">${props.price}</div>
       </div>
     </>
   );
