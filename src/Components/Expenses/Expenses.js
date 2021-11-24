be25f057ca95faf4f5ef1259bb325ff5490edebf
Mananjay Shrestha <Mananjaysth111@gmail.com>
2021-11-24 21:07:36 +0545

    state-in-react

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+// useState is a hook which is import from react libary
+// we can reconized the hook by looking at start value which start with always use
 import "./Expenses.css";
 import Expensesdate from "./Expensesdate";
 const Expenses = (props) => {
@@ -7,14 +9,30 @@ const Expenses = (props) => {
   // now access the value like as object properties
   // {props.title}
 
+  // Now here to change the value of certain div or tag we have to used useState to hold the value and change the value
+  // useState is a function which return the array that hold two value where first one is a variable which is store
+  // the current value likewise second one is a function which help to change the value or update the value.for instance
+  const [title, setTitle] = useState(props.title);
+
+  // make a function to add event to have change on click
+  const changeTitle = () => {
+    // Now to make any change on event we have to call useState function
+    setTitle("New Title added soon...");
+  };
+
   return (
     <>
       <div className="container">
         <Expensesdate time={props.time} />
         {/* making  a multiple compoent where one certain div with name expense-date is written in expensedate component 
         and import in expenses.js component */}
-        <div className="expense-title">{props.title}</div>
+        <div className="expense-title">{title}</div>
+        {/* here the title is a varibale which store the props.title */}
         <div className="expense-price">${props.price}</div>
+        <button className="changebtn" onClick={changeTitle}>
+          Title Change
+        </button>
+        {/* here in the button to add a event we have to add a function in onclick event same like in javascript */}
       </div>
     </>
   );
